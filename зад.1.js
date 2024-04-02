a1ecca935793dc48260e923b38eb98c2a758c5a7
Ruslana-Po <161792415+Ruslana-Po@users.noreply.github.com>
2024-04-02 18:42:25 +0700

    зад.1.js

---
+++ зад.1.js
@@ -0,0 +1,39 @@
+// Функция проверки на простоту
+function findPrime(originalNumber) {
+    // Проверка простоты
+    for (let i = 2; i <= Math.sqrt(originalNumber); i++) {
+        if (originalNumber % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Функция для поиска ближайшего простого числа
+function nearestPrimeNumber(originalNumber) {
+    let leftward = originalNumber;
+    let right = originalNumber ++;
+    while (true) {
+        if (findPrime(leftward)) {
+            return leftward;
+        } else if (findPrime(right)) {
+            return right;
+        }
+        leftward--;
+        right++;
+    }
+}
+
+// Главная функция
+
+    let number = prompt("Введите число больше 1: ");
+    let primeNumber; 
+     while(number < 0){
+     number = prompt("Не верно введено значение, введите заново : ");
+    }
+    if(number===0 || number===1){
+        primeNumber =2;
+    }else{
+        primeNumber = nearestPrimeNumber(number);
+    }
+    console.log(Math.abs(number-primeNumber));
