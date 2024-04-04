e878fd1ca48adc625492b9b60bca736f9b8fb677
Ruslana-Po <161792415+Ruslana-Po@users.noreply.github.com>
2024-04-04 16:51:06 +0700

    зад2.cpp

---
+++ зад2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <Windows.h>
+#include<vector>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Введите количество: ";
+    int n;
+    cin >> n;
+    vector<int> number(n);
+    cout << "Введите числа: ";
+    //Ввод чисел в масиве
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        // Проверка
+        if (cin.fail()) {
+            cout << "Не верное значение";
+            return 0;
+        }
+        number[i] = x;
+    }
+    //Переворачиваем числа
+    for (int i = 0; i < n; i++) {
+        int x = number[i];
+        int result = 0;
+        while (x != 0) {
+            result = result * 10 + (x % 10);
+            x = x / 10;
+        }
+        number[i] = result;
+    }
+    //Вывод
+    for (int i = 0; i < n; i++) {
+        cout << number[i] << " ";
+    }
+}
